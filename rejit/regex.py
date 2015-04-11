fdd0622011bde580b895fe222282f3a6219013b7
ziowk <ziowkftw@gmail.com>
2015-04-11 22:28:42 +0200

    Add DFA-to-NFA conversion algorithm
    A DFA can only be created from a NFA object. The conversion algorithm is
    implemented in the DFA's `__init__` method. It uses powerset
    construction approach adjusted to rejit's NFA implementation.
    
    Closes #18

--- rejit/regex.py
+++ rejit/regex.py
@@ -723,6 +723,66 @@ class NFA:
         return n
 
 class DFA:
+    def __init__(self, nfa):
+        # all nfa's states reachable from the start state
+        nfa_states = frozenset(NFA._get_all_reachable_states(nfa._start))
+
+        # nfa described without use of epsilon edges.
+        # nfa_state_edges is constructed as {state_num: {char: {state_num}}}
+        # which maps state_num to dictionaries which map each char to 
+        # a set of state_nums which are reachable from the state using that char
+        nfa_state_edges = {
+                st._state_num: DFA._nfa_char2statenum_set(st)
+                for st in nfa_states
+            }
+
+        # newstates is an early representation of the constructed DFA
+        # newstates is constructed as {multiname: {char: {state_num}}} where
+        # multiname key is a string which describes a set of states from the nfa
+        # and it points to a dictionary which map each char to a set of 
+        # state_nums which are reachable from the set of states using that char.
+        # newstates is initialized with multistates which correspond to 
+        # the nfa's states.
+        newstates = {DFA._multistate_name({num}): c2s for num,c2s in nfa_state_edges.items()}
+
+        # m2ss dict maps multiname to a corresponding state_num set
+        m2ss = {DFA._multistate_name({num}): {num} for num in nfa_state_edges}
+
+        # toadd contains sets of state_nums which are multistates to be added to newstates
+        toadd = [s for st,d in nfa_state_edges.items() for char,s in d.items()]
+
+        while toadd:
+            states = toadd.pop()
+            name = DFA._multistate_name(states)
+            # check if they are already added, if not create a multistate by merging singlestates
+            if name not in newstates:
+                _, c2s = DFA._merge_states(states,nfa_state_edges)
+                newstates[name] = c2s
+                m2ss[name] = states
+                # merging multistate could create new multistates to add 
+                for char,s in c2s.items():
+                    toadd.append(s)
+
+        # convert newstates to {multiname:{char:multiname}} dict
+        for st,c2s in newstates.items():
+            for c,s in c2s.items():
+                c2s[c] = DFA._multistate_name(s)
+
+        # end states are multistates which contain nfa._end state ...
+        end_states = set(filter(lambda st: nfa._end._state_num in m2ss[st], newstates))
+        # ... and singlestates from nfa which can reach nfa._end with epsilon-moves
+        end_states |= set(map(
+            lambda st: DFA._multistate_name({st._state_num}), 
+            filter(lambda st: nfa._end in NFA._moveEpsilon({st}), nfa_states)))
+
+        # save relevant data
+        # start state
+        self._start = DFA._multistate_name({nfa._start._state_num})
+        # DFA state/edge dict
+        self._states_edges = newstates
+        # set of accepting states
+        self._end_states = frozenset(end_states)
+
     @staticmethod
     def _nfa_reachable_noneps_edges(state):
         return filter(lambda e: e[0], functools.reduce(lambda x,y: x+y,[s._edges for s in NFA._moveEpsilon({state})]))
