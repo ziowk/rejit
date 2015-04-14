64d4be76d36f87dc09c69acd7c12b61ac5d87342
ziowk <ziowkftw@gmail.com>
2015-04-14 16:55:12 +0200

    Add simple setup.py installation file
    Added `setup.py` file. It uses `setuptools` package to perform
    installation of the package. `setup.py` is filled out with basic
    information about the package.
    
    Part of #52

---
+++ setup.py
@@ -0,0 +1,29 @@
+#encoding: utf8
+
+from setuptools import setup
+
+setup(  
+    name='rejit',
+    version='0.1',
+    description='Simple regular expression just-in-time compiler',
+    author='Grzegorz Ludwikowski',
+    author_email='ludwikowskig@gmail.com',
+    url='https://github.com/ziowk/rejit',
+    license='GPLv2',
+    packages=['rejit',],
+    extras_require = {
+        'Graphviz': ["graphviz>=0.4.3"],
+        },
+    keywords = ['regexp', 'regex', 'JIT', 'just-in-time', 'compiler'],
+    classifiers = [
+        'Develeopment Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Text Processing',
+        ],
+    zip_safe = True,
+)
+
