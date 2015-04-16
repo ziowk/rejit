71d817eef1d1a86bef427ebc1bbb7dcc2ca8c556
ziowk <ziowkftw@gmail.com>
2015-04-16 23:39:11 +0200

    Add executable memory allocation for GNU/Linux
    `windows_loadcode.h` equivalent for GNU/Linux.
    
    `gnu_linux_loadcode.h` allows to allocate executable memory and copy JIT
    compiled code into it. Supports only GNU/Linux. Not included in
    compilation yet.
    
    Part of #74

---
+++ rejit/gnu_linux_loadcode.h
@@ -0,0 +1,45 @@
+#include <string.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+/*----- jit declarations -----*/
+
+typedef int(*JITFunc)(const char* string, size_t length);
+
+typedef struct {
+    JITFunc func;
+    size_t length;
+} FunObj;
+
+FunObj* createFunction(const void* source, size_t source_size);
+
+/* ----- jit definitions -----*/
+
+FunObj * 
+createFunction(const void* source, size_t source_size) {
+	int oldProtect;
+    FunObj *funobj;
+
+	// allocate executable and writeable memory
+    void* ptr = mmap(0, source_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (ptr == MAP_FAILED) {
+		return NULL;
+	}
+
+	// copy the code
+	memcpy(ptr, source, source_size);
+
+	// set protection to executable
+    if (mprotect(ptr, source_size, PROT_READ | PROT_EXEC) == -1) {
+        return NULL;
+    }
+
+    // create FunObj object
+    if ((funobj = (FunObj*)malloc(sizeof(FunObj))) == NULL) {
+        return NULL;
+    }
+    funobj->func = ptr;
+    funobj->length = source_size;
+	return funobj;
+}
+
