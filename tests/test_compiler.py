e1694883a1bece4fa907f64db298cd7382708273
ziowk <ziowkftw@gmail.com>
2015-04-22 23:19:50 +0200

    Add standard regex matcher tests for JITMatcher
    JITMatcher is tested against standard regex matcher test suite.
    
    Closes #72

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -1,10 +1,11 @@
 #encoding: utf8
 
 from rejit.dfa import DFA
-from tests.helper import accept_test_helper
-from rejit.compiler import VMRegex
 import rejit.loadcode as loadcode
 import rejit.compiler as compiler
+from rejit.compiler import VMRegex
+from rejit.compiler import JITMatcher
+from tests.helper import accept_test_helper
 
 import tests.automaton_test_cases as auto_cases
 
@@ -70,4 +71,52 @@ class TestCodeGen:
         # test for a bug fixed in 639968d
         _, binary = compiler.encode_instruction([0x8B], reg=compiler.Reg.EAX, reg_mem=compiler.Reg.EAX)
         assert binary == b'\x8B\xC0' 
+
+class Testx86accept:
+    def test_empty_JITMatcher(self):
+        accept_test_helper(JITMatcher(DFA(auto_cases.empty_nfa)), auto_cases.empty_cases)
+
+    def test_symbol_JITMatcher(self):
+        for nfa,cases in zip(auto_cases.symbol_nfas, auto_cases.symbol_cases):
+            accept_test_helper(JITMatcher(DFA(nfa)), cases)
+
+    def test_any_JITMatcher(self):
+        accept_test_helper(JITMatcher(DFA(auto_cases.any_nfa)),auto_cases.any_cases)
+
+    def test_none_JITMatcher(self):
+        accept_test_helper(JITMatcher(DFA(auto_cases.none_nfa)),auto_cases.none_cases)
+
+    def test_kleene_JITMatcher(self):
+        accept_test_helper(JITMatcher(DFA(auto_cases.kleene_nfa)),auto_cases.kleene_cases)
+
+    def test_kleene_plus_JITMatcher(self):
+        accept_test_helper(JITMatcher(DFA(auto_cases.kleene_plus_nfa)),auto_cases.kleene_plus_cases)
+
+    def test_concat_JITMatcher(self):
+        accept_test_helper(JITMatcher(DFA(auto_cases.concat_nfa_1)),auto_cases.concat_cases_1)
+        accept_test_helper(JITMatcher(DFA(auto_cases.concat_nfa_2)),auto_cases.concat_cases_2)
+
+    def test_concat_many_JITMatcher(self):
+        accept_test_helper(JITMatcher(DFA(auto_cases.concat_many_nfa_1)),auto_cases.concat_many_cases_1)
+        accept_test_helper(JITMatcher(DFA(auto_cases.concat_many_nfa_2)),auto_cases.concat_many_cases_2)
+        accept_test_helper(JITMatcher(DFA(auto_cases.concat_many_nfa_3)),auto_cases.concat_many_cases_3)
+
+    def test_union_JITMatcher(self):
+        accept_test_helper(JITMatcher(DFA(auto_cases.union_nfa)),auto_cases.union_cases)
+
+    def test_union_many_JITMatcher(self):
+        accept_test_helper(JITMatcher(DFA(auto_cases.union_many_nfa_1)),auto_cases.union_many_cases_1)
+        accept_test_helper(JITMatcher(DFA(auto_cases.union_many_nfa_2)),auto_cases.union_many_cases_2)
+        accept_test_helper(JITMatcher(DFA(auto_cases.union_many_nfa_3)),auto_cases.union_many_cases_3)
+
+    def test_char_set_JITMatcher(self):
+        accept_test_helper(JITMatcher(DFA(auto_cases.char_set_nfa_1)),auto_cases.char_set_cases_1)
+        accept_test_helper(JITMatcher(DFA(auto_cases.char_set_nfa_2)),auto_cases.char_set_cases_2)
+
+    def test_zero_or_one_JITMatcher(self):
+        accept_test_helper(JITMatcher(DFA(auto_cases.zero_or_one_nfa)),auto_cases.zero_or_one_cases)
+
+    def test_complex_JITMatcher(self):
+        accept_test_helper(JITMatcher(DFA(auto_cases.complex_nfa_1)),auto_cases.complex_cases_1)
+        accept_test_helper(JITMatcher(DFA(auto_cases.complex_nfa_2)),auto_cases.complex_cases_2)
 
