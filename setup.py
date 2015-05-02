dbd9928c6c917bbf339b3b8316a4f8e991de88e9
ziowk <ziowkftw@gmail.com>
2015-05-02 21:58:13 +0200

    Bump version number to v0.2
    v0.2 introduces the initial implementation of regex JIT compilation. See
    README.md for supported platforms and new features.
    
    Closes #85

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, Extension
 
 setup(  
     name='rejit',
-    version='0.1',
+    version='0.2',
     description='Simple regular expression just-in-time compiler',
     author='Grzegorz Ludwikowski',
     author_email='ludwikowskig@gmail.com',
