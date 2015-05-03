4c0d0f20410d796e1883f096199eca4413cab647
ziowk <ziowkftw@gmail.com>
2015-05-03 22:15:09 +0200

    Fix SIBByte input validation bug
    `SIBByte.__init__` checks its arguments if they are in the correct
    range.  The range for `index` argument was too small - two bits instead
    of three.

--- rejit/compiler.py
+++ rejit/compiler.py
@@ -803,9 +803,9 @@ class ModRMByte:
                 )
 
 class SIBByte:
-    def __init__(self, base=0, index=0, scale=0):
+    def __init__(self, *, base=0, index=0, scale=0):
         assert 0 <= base <= 0b111
-        assert 0 <= index <= 0b11
+        assert 0 <= index <= 0b111
         assert 0 <= scale <= 0b11
         self._byte = scale << 6 | index << 3 | base
 
